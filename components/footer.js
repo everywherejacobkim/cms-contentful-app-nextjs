44c8ba78f9157871b1d4653dd7fba8b58b00f805
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-04-02 23:32:34 -0700

    Build: create layout & set tailwind css

--- components/footer.js
+++ components/footer.js
@@ -7,20 +7,20 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Skillfully crafted by developer Kim.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href="https://www.linkedin.com/in/everywherejacobkim"
+              className="mx-3 bg-blue-800 hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Visit LinkedIn
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href="https://github.com/everywherejacobkim"
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              View GitHub
             </a>
           </div>
         </div>
